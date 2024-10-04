01b227bfbe9b3b938bb92269afd6397a817d0940
Orlando <yandelmartinez864@gmail.com>
2024-10-04 19:52:03 -0400

    Añadiendo archivos claves

--- src/main.js
+++ src/main.js
@@ -1,20 +1,31 @@
-const {BrowserWindow} = require('electron')
-let window
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
 
-function createWindow() {
-    const window = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true
-      }
-    })
-    window.loadFile('src/index.html');
-  
-    return window;
-  }
-  
-  module.exports = {
-    createWindow
-  }
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
